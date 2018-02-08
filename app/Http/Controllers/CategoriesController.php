1f6330be3abee868a4757b2d29cefba5beea83d0
vagrant <vagrant@homestead>
2018-02-08 14:59:26 +0000

    话题排序完成，用到了排序逻辑本地作用域

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -10,11 +10,11 @@ class CategoriesController extends Controller
 {
     //
 
-    public function show(Category $category)
+    public function show(Request $request,Category $category,Topic $topic)
     {
         #{{ route('categories.show',$topic->category->id) }}  视图点击
         // 读取分类 ID 关联的话题，并按每 20 条分页
-        $topics = Topic::where('category_id', $category->id)->paginate(20);
+        $topics = $topic->WithOrder($request->order)->where('category_id', $category->id)->paginate(20);
         // 传参变量话题和分类到模板中
         return view('topics.index', compact('topics', 'category'));
     }
