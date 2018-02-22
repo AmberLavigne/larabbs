dc037b73e23828a0916eb1e3bbb9b5a25f485ea1
vagrant <vagrant@homestead>
2018-02-22 01:03:53 +0000

    用户可以创建话题

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -8,19 +8,17 @@ class TopicRequest extends Request
     {
         switch($this->method())
         {
+            #表单方法 POST, PUT, PATCH 使用的是相同的一套验证规则。
             // CREATE
             case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
             // UPDATE
             case 'PUT':
             case 'PATCH':
             {
                 return [
-                    // UPDATE ROLES
+                    'title'       => 'required|min:2',
+                    'body'        => 'required|min:3',
+                    'category_id' => 'required|numeric',
                 ];
             }
             case 'GET':
@@ -36,6 +34,8 @@ class TopicRequest extends Request
     {
         return [
             // Validation messages
+            'title.min' => '标题必须至少两个字符',
+            'body.min' => '文章内容必须至少三个字符',
         ];
     }
 }
