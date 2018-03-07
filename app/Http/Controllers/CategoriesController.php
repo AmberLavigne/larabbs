3affea580c8a98b13e3831f7a0e10dc42594d099
vagrant <vagrant@homestead>
2018-03-07 13:09:08 +0000

    添加侧边栏导航链接

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,19 +6,21 @@ use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Category;
 use App\Models\User;
+use App\Models\Link;
 
 class CategoriesController extends Controller
 {
     //
 
-    public function show(Request $request,Category $category,Topic $topic,User $user)
+    public function show(Request $request,Category $category,Topic $topic,User $user,Link $link)
     {
         #{{ route('categories.show',$topic->category->id) }}  视图点击
         // 读取分类 ID 关联的话题，并按每 20 条分页
         $topics = $topic->WithOrder($request->order)->where('category_id', $category->id)->paginate(20);
         // 活跃用户列表
         $active_users = $user->getActiveUsers();
+        $links = $link->getAllCached();
         // 传参变量话题和分类到模板中
-        return view('topics.index', compact('topics', 'category','active_users'));
+        return view('topics.index', compact('topics', 'category','active_users','links'));
     }
 }
